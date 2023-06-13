c78eb20b8df754e565dbc664c1cab00b8391e219
Leonardo Lopes <leuuu1010@gmail.com>
2023-06-13 09:52:37 -0300

    Desenvolvendo Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,19 @@
-const Header = () => {
+const Header = (props) => {
+function handleSubmit(event){
+  event.preventDefault();
+  const searchValue = event.target[0].value;
+  props.onSubmit(searchValue)
+  event.target[0].value = "";
+
+}
+
   return (
-    <div>Header</div>
+    <header className="Header">
+    <h1>Dnc Movie List</h1>
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Pesquise por filmes" />
+    </form>
+    </header>
   );
 };
 
